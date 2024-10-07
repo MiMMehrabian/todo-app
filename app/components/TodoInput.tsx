2c16d22d15a9d894ac51acd9b69356929ebc08d8
MiMMehrabian <mim79mehrabian@gmail.com>
2024-10-07 11:39:30 +0330

    fix : fix ui bugs

--- app/components/TodoInput.tsx
+++ app/components/TodoInput.tsx
@@ -14,19 +14,19 @@ const TodoInput: React.FC<TodoInputProps> = ({
   handleAddTodo,
   loading,
 }) => (
-  <form onSubmit={handleAddTodo} className="flex mb-4">
+  <form onSubmit={handleAddTodo} className="flex flex-col sm:flex-row mb-4">
     <input
       type="text"
       value={inputValue}
       onChange={(e) => setInputValue(e.target.value)}
       placeholder="Add a new todo"
       disabled={loading}
-      className="flex-grow border border-gray-300 rounded-l-lg p-2 focus:outline-none focus:ring-2 focus:ring-blue-400"
+      className="flex-grow border border-gray-300 sm:rounded-l-lg rounded-t-lg sm:rounded-tr-none p-2 focus:outline-none focus:ring-2 focus:ring-blue-400"
     />
     <button
       type="submit"
       disabled={loading}
-      className="bg-blue-500 text-white rounded-r-lg px-4 hover:bg-blue-600 transition"
+      className="bg-blue-500 text-white sm:rounded-r-lg rounded-b-lg sm:rounded-bl-none px-4 py-1 sm:py-0 hover:bg-blue-600 transition"
     >
       Add
     </button>
